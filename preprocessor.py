29203f173acf3c5c46f0e6cf8b0f08d370fbd152
Kushail <kushailsingh98@gmail.com>
2020-06-12 01:56:47 +0200

    creates list of tuples with each tuple [date][debate][content]

--- preprocessor.py
+++ preprocessor.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
+import time
 
+t0 = time.time()
 with open('bigFile.xml') as raw_file:
-    soup = BeautifulSoup(raw_file, 'lxml')
-    debateBody = soup.debatebody   # tag object, its the parent tag of all debatesections called debatebody
-    #results = debateBody.find_all('debatesection', recursive = False) # returns array of each parent debatesection
-    
+    entire_file_soup = BeautifulSoup(raw_file, 'lxml')
+    debateBody_tag = entire_file_soup.debatebody
+    data = []
 
-    output2 = open("output2.xml", "w")
-    
-    #for child in results
-    for debateSection in results[0].children
-        print(debateSection)
-        output.write(str(debateSection))
-
-
-
-
-
-
-    
+    for debateSection in debateBody_tag.children:
+        day_of_debates_soup = BeautifulSoup(str(debateSection), 'lxml')
+        day = day_of_debates_soup.heading
+        if day:
+            day = "".join(day)
+        for inside in day_of_debates_soup.children:
+            specific_debate_soup = BeautifulSoup(str(inside), 'lxml')
+            for tag in specific_debate_soup.debatesection.contents:
+                if str(tag.name) == 'debatesection':
+                    title = "".join(tag.contents[1])
+                    content = str(tag.find_all('p'))
+                    tupleofshit = (day,title,content)
+                    data.append(tupleofshit)
+t1 = time.time()
+total = t1-t0
+print(total)
